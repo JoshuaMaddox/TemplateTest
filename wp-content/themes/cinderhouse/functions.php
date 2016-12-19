ca4d9b573acdee9aabe28722893a1b1350ecc482
Joshua Maddox <joshua@cinderhousecreative.com>
2016-12-19 20:57:32 +0800

    added styles from portfolio theme and added first section to index.php

--- wp-content/themes/cinderhouse/functions.php
+++ wp-content/themes/cinderhouse/functions.php
@@ -1,13 +1,11 @@
 <?php  
-
-  /* Adding Bootstrap 3 to Theme */
-
-  function theme_add_bootstrap() {
+  /* Adding Bootstrap 3 to Theme  with the custom function theme_add_bootstrap_styles and the add_action function*/
+  function theme_add_bootstrap_styles() {
     wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/css/bootstrap.min.css' );
     wp_enqueue_style( 'style-css', get_template_directory_uri() . '/style.css' );
     wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js' );
   }
 
-  add_action( 'wp_enqueue_scripts', 'theme_add_bootstrap' );
-
+  //uses wp_enqueue_scripts along with our custom function
+  add_action( 'wp_enqueue_scripts', 'theme_add_bootstrap_styles' );
 ?>
--- wp-content/themes/cinderhouse/index.php
+++ wp-content/themes/cinderhouse/index.php
@@ -1,28 +1,57 @@
 <?php  
 /* Main Template File */
-
-//Gets the header file - the header partial
+wp_head();
+/* Gets the header file - the header partial */
 get_header();
-
-
-//Will pull in the content and the heading of what is being queried
-
 ?>
 
-<div class="cinderhouse-theme-main-wrapper">
-  <div class="row">
-    <div class="col-md-6">
-      <h2>Cinderhouse Warm-up Theme</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel eum omnis error porro earum dicta assumenda, molestiae praesentium aspernatur et, laborum dolore, magnam aliquid maiores distinctio doloribus quasi necessitatibus accusamus.</p>  
-    </div>
-    <div class="col-md-6">
-      <h2>Cinderhouse Warm-up Theme</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel eum omnis error porro earum dicta assumenda, molestiae praesentium aspernatur et, laborum dolore, magnam aliquid maiores distinctio doloribus quasi necessitatibus accusamus.</p>  
-    </div>
-  </div>
-</div> 
+<!-- Will pull in the content and the heading of what is being queried -->
+<section class="jm-resume" id="section2" >
+  <div class="container-fluid">
+    <div id="resumeBox" class="row">
+      <div class="col-sm-6 jm-resume-toggle-one">
+        <div  class="jm-resume-box">
+          <h2><span class="jm-bordered-heading"> '07 — Current Cinderhouse </span></h2>
+          <h4>What I Do</h4>
+          <p>Cinderhouse is an ongoing pet project that began as a creative outlet and testing ground for marketing and design concepts but turned into a successful social enterprise. I am a co-founder, active designer / marketer and floor sweeper. </p> 
+          <h4>Why it's Relevant</h4>
+          <p>Cinderhouse allows us to imagine, test, fail and succeed at the concepts we're normally not allowed to play with while in more civilized companies. All of the experimenting and learning we do benefits the companies we work for both from the knowledge we gain and in that we expel some of our least controlled creative urges. Cinderhouse uses profit from the work we do to test, found and fund social enterprise structures that use 100% of profit to battle poverty and inequality.</p>
+        </div><!--JM-RESUME-BOX-->
+      </div><!--COL-->
+      <div  class="col-sm-6 jm-resume-toggle-two">
+        <div class="jm-resume-box">
+          <h2><span class="jm-bordered-heading"> '09 — '15 British School Nanjing </span></h2>
+          <h4>What I Did</h4>
+          <p>As Head of Administration, I was directly responsible for a staff of 19, marketing and all non-academic management including campus oversight and contract negotiations for one of the most esteemed International schools in Nanjing, China.</p> 
+          <h4>Why it's Relevant</h4>
+          <p>I wrote and implemented creative marketing strategies that grew revenue by 350%, successfully oversaw a new 15,000 SQM campus building project, restructured administration management to save 125K+ USD annually while improving efficacy AND once, during land contract negotiations with top level city officials, ate live shrimp soaked in wine without retching(mainly). The lessons I learned while at BSN were far greater than just knowing that one should start diner meetings with a lie about being allergic to shellfish.</p>
+        </div><!--JM-RESUME-BOX-->
+      </div><!--COL-->
+    </div><!--ROW-->
+    <div class="row">
+      <div class="col-sm-6 jm-resume-toggle-three">
+        <div class="jm-resume-box">
+          <h2><span class="jm-bordered-heading">'08 — '09 Hongren</span></h2>
+          <h4>What I Did</h4>
+          <p>I was hired as the Director of Business Development and was responsible for designing and implementing marketing and general business development initiatives in the support of sales. I was promoted to head of the campus program shortly after starting. </p> 
+          <h4>Why it's Relevant</h4>
+          <p>I was responsible for a staff of 24, a 12,000 SQM+ campus and over 400 live-in students. As Chinese was the main language used for management, I had to accelerate my Mandarin studies and subject myself to good hearted but well deserved ridicule of my language and cultural inadequacies. Although, I learned a lot about day to day management, marketing the program, working with clients etc...the most valuable lessons came in learning how to work with staff of an array of nationationalities and cultures while equitably navigating the conflicts and misunderstandings that arose.</p>
+        </div><!--JM-RESUME-BOX-->
+      </div><!--COL-->
+      <div class="col-sm-6 jm-resume-toggle-four">
+        <div class="jm-resume-box">
+          <h2><span class="jm-bordered-heading"> '06 — '08 JBM</span></h2>
+          <h4>What I Did</h4>
+          <p>Founded a soft-skills consultancy in Beijing during the lead-up to the 2008 Olympics. We managed client accounts around Beijing from the Central Business District to Shangdi(Beijing's Silicon Valley). I became well acquainted with the 48 hour work day.</h4>
+          <h4>Why It's Relevant</h4>
+          <p>We advised companies such as IBM-Lenovo, LG, Langham Hotels, Elong.com, Match.com etc... in soft-skills management and business communication towards their overseas counterparts and clients. Because our services had to be tailored according to individual department needs, I was allowed to peer directly into and learn from an array of international management structures. I also learned what it means to fight for life as a small start-up in a saturated market via creative marketing and winning contracts by converting client problems into concierge service offerings.</p>
+        </div><!--JM-RESUME-BOX-->
+      </div><!--COL-->
+    </div><!--ROW-->
+  </div><!--CONTAINER-->
+</section>
 
 <?php 
-  //Gets the footer file - the footer partial 
+  /* Gets the footer file - the footer partial  */
   get_footer();
 ?>
